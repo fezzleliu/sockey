a69ad7d524456c5391ea8db734f50adf454d466c
Joshua Liu <supajoshjunk@gmail.com>
2022-07-20 13:15:03 -0400

    some bug fixes (#12)
    * its done! yaya
    
    * fix font issue
    
    * fix build issue

--- client/src/start-screen.ts
+++ client/src/start-screen.ts
@@ -4,15 +4,17 @@ import './start.scss';
 import playerBlue from '../assets/player_blue.png';
 // @ts-ignore
 import playerRed from '../assets/player_red.png';
-import { format } from 'path';
 
 document.querySelector('.outer .btns .btn.how').addEventListener('click', () => {
-	const modal = createModal('How to play', 'just know');
+	const modal = createModal('How to play', `
+On a computer, hold down the the mouse and drag to move. Hold the space bar to charge the ball, and release to shoot.<br>
+On a touchscreen device, use the joystick to move. Hold the arrow button to charge the ball, and release to shoot.
+`);
 	document.querySelector('.start-outer').appendChild(modal);
 });
 
 document.querySelector('.outer .btns .btn.about').addEventListener('click', () => {
-	const modal = createModal('About this game', 'made by joshua liu');
+	const modal = createModal('About this game', 'Sockey is an open source game created by Joshua Liu. It was built with TypeScript and Socket.io, with development and build with Vite.');
 	document.querySelector('.start-outer').appendChild(modal);
 });
 
